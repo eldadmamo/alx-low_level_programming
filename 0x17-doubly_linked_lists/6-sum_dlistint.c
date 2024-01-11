b1059d8b1db8f7248f3029eea8719456dca079a3
Eldad <eldadf456@gmail.com>
2024-01-11 13:10:49 -0800

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data 
- * @head: head list
- * Return: sum data
+ * sum_dlistint - returns the sum of all the data (n)
+ * of a doubly linked list
+ *
+ * @head: head of the list
+ * Return: sum of the data
  */
 int sum_dlistint(dlistint_t *head)
 {
